e3eec29747d2bd7f1200cc52ea9663302db56f98
Olivia Grace Telford <ogtelford12@gmail.com>
2015-02-26 17:34:43 -0800

    Modified SQL query and parameters file to include [alpha/M]

--- InitialExperiments/getParameters.sql
+++ InitialExperiments/getParameters.sql
@@ -1,4 +1,5 @@
-SELECT a.param_m_h, a.teff, a.logg, b.ra
+SELECT a.param_m_h, a.teff, a.logg, a.param_alpha_m, b.ra
 FROM aspcapStar as a,apogeeStar as b
 WHERE a.apstar_id = b.apstar_id AND b.location_id = 4558 
 ORDER BY b.ra
+
